03ec0aa678d11c6d1a94b654b2bdc25cc0925ee9
Alessio Rocco <alessio.rocco.lt@gmail.com>
2014-01-20 12:47:56 +0100

    updates version

--- lib/moonshine/version.rb
+++ lib/moonshine/version.rb
@@ -1,3 +1,3 @@
 module Moonshine
-  VERSION = "0.0.1"
+  VERSION = "0.0.1.pre"
 end
--- spec/lib/moonshine/version_spec.rb
+++ spec/lib/moonshine/version_spec.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 describe Moonshine::VERSION do
 
-  it 'is 0.0.1' do
-    Moonshine::VERSION.must_equal '0.0.1'
+  it 'is 0.0.1.pre' do
+    Moonshine::VERSION.must_equal '0.0.1.pre'
   end
 end
