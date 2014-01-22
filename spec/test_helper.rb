3710d5e1343b638889ba4a0e3b584511a30b657b
Alessio Rocco <alessio.rocco.lt@gmail.com>
2014-01-22 15:50:13 +0100

    refactorings mock classes

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -4,9 +4,28 @@ require 'mocha/mini_test'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'coveralls'
-require 'support/mock_default_subject'
-require 'support/mock_subject'
-require 'support/mock_chain_builder'
 
 # Code coverage
 Coveralls.wear!
+
+class MiniTest::Spec
+  before :each do
+    ::MockSubject = Class.new
+    ::MockDefaultSubject = Class.new do
+      def scope_method value
+      end
+    end
+    ::MockChainBuilder = Class.new(Moonshine::Base) do
+      subject MockDefaultSubject
+
+      def clean value
+      end
+    end
+  end
+
+  after :each do
+    Object.send(:remove_const, 'MockChainBuilder')
+    Object.send(:remove_const, 'MockSubject')
+    Object.send(:remove_const, 'MockDefaultSubject')
+  end
+end
