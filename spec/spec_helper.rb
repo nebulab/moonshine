20952337935752a8f53519f62358c14b98883d20
Alessio Rocco <alessio.rocco.lt@gmail.com>
2014-01-28 17:00:33 +0100

    removes not needed code from spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,23 +9,4 @@ require 'mocha/mini_test'
 require 'minitest/pride'
 
 class MiniTest::Spec
-  before :each do
-    ::MockSubject = Class.new
-    ::MockDefaultSubject = Class.new do
-      def self.scope_method value
-      end
-    end
-    ::MockChainBuilder = Class.new(Moonshine::Base) do
-      subject MockDefaultSubject
-
-      def clean value
-      end
-    end
-  end
-
-  after :each do
-    Object.send(:remove_const, 'MockChainBuilder')
-    Object.send(:remove_const, 'MockSubject')
-    Object.send(:remove_const, 'MockDefaultSubject')
-  end
 end
