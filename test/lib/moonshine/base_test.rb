660f23cc66888eea5b95459e5aa72db547252d4d
Alessio Rocco <alessio.rocco.lt@gmail.com>
2014-06-27 16:08:39 +0200

    adds tests to refactored base class

--- test/lib/moonshine/base_test.rb
+++ test/lib/moonshine/base_test.rb
@@ -1,91 +1,68 @@
-require 'test_helper'
+ require 'test_helper'
 
 describe Moonshine::Base do
-  before(:each) do
-    @chain_builder = Class.new(Moonshine::Base)
-    @default_subject = mock('default_subject')
-  end
 
-  describe '.subject' do
-    before do
-      @chain_builder.default_subject = @default_subject
+  let(:tale) { Tale.new }
+  let(:tale_generator){ 
+    empty_tale_generator = EmptyTaleGenerator.new({ beginning: true }) 
+    empty_tale_generator.subject = tale
+    empty_tale_generator
+  }
+
+  describe '#add_filter_to_chain' do
+
+    let(:params) { [:beginning] }
+
+    it 'instantiates a Moonshine::Filter' do
+      Moonshine::Filter.expects(:new).with(*params.push({}))
+      tale_generator.add_filter_to_chain(:beginning)
     end
 
-    it 'sets default subject class attribute' do
-      @chain_builder.default_subject.must_equal @default_subject
+    describe 'when params have options' do
+    it 'instantiates a Moonshine::Filter with options' do
+      Moonshine::Filter.expects(:new).with(*params.push({as_boolean: true}))
+      tale_generator.add_filter_to_chain(:beginning, as_boolean: true)
     end
 
-    describe 'when it is initialized without a subject' do
-      it 'sets subject as default subject' do
-        @chain_builder.new({}).subject.must_equal @default_subject
+    end
+
+    describe 'when params have a block' do
+      it 'instantiates a Moonshine::Filter with a block' do
+        a_block = Proc.new {}
+        Moonshine::Filter.expects(:new).with(*params.push({}, &a_block))
+        tale_generator.add_filter_to_chain(:beginning, &a_block)
       end
     end
 
-    describe 'when it is initialized with a subject' do
-      let(:subject){ mock('subject') }
-
-      it 'sets subject as requested' do
-        @chain_builder.new({}, subject).subject.must_equal subject
-      end
-    end
-  end
-
-  describe '.param' do
-    it 'instantiates a Moonshine::Filter class' do
-      Moonshine::Filter.expects(:new).with(:filter_name, method_name: :method)
-      @chain_builder.param :filter_name, call: :method
-    end
-
-    describe 'when :call is not given' do
-      it 'instantiates a Moonshine::Filter class with call same as first param' do
-        Moonshine::Filter.expects(:new).with(:filter_name, method_name: :filter_name)
-        @chain_builder.param :filter_name
-      end
-
-      describe 'and a block is given' do
-        it 'instantiates a Moonshine::Filter call with block as call' do
-          block = Proc.new{ |subject| subject }
-          Moonshine::Filter.expects(:new).with(:filter_name, &block)
-          @chain_builder.param :filter_name, &block
-        end
-      end
-    end
-
-    it 'adds filter to default_chain' do
-      filter =  mock('filter')
+    it 'adds a Moonshine:Filter instance to filter_chain' do
+      filter = mock('a filter')
       Moonshine::Filter.stubs(:new).returns(filter)
-      @chain_builder.param :filter_name, call: :scope
-      @chain_builder.default_chain.must_equal [filter]
-    end
-  end
-
-  describe '#add' do
-    it 'adds filter to chain' do
-      filter = mock('filter')
-      chain_builder_instance = @chain_builder.new({})
-      chain_builder_instance.add(filter)
-      chain_builder_instance.chain.must_equal [filter]
+      tale_generator.add_filter_to_chain(:beginning)
+      tale_generator.chain.must_include(filter)
     end
   end
 
   describe '#run' do
+    let(:tale_generator){ 
+      empty_tale_generator = EmptyTaleGenerator.new({ beginning: true, conclusion: true }) 
+      empty_tale_generator.subject = tale
+      empty_tale_generator
+    }
 
-    it 'run execute on each filter' do
-      filter1 = mock('filter1')
-      filter1.stubs(:name).returns(:filter1)
-      filter2 = mock('filter2')
-      filter2.stubs(:name).returns(:filter2)
-      params = [filter1, filter2]
-      @chain_builder.default_chain = params
-      chain_builder_instance = @chain_builder.new({ filter1: 1, filter2: 2})
-      filter1.expects(:execute).with(chain_builder_instance)
-      filter2.expects(:execute).with(chain_builder_instance)
-      chain_builder_instance.run
+    before do
+      tale_generator.add_filter_to_chain(:beginning, as_boolean: true)
+      tale_generator.add_filter_to_chain(:conclusion, as_boolean: true)
     end
 
-    it 'returns subject' do
-      chain_builder_instance = @chain_builder.new({})
-      chain_builder_instance.run.must_equal chain_builder_instance.subject
+    it 'sends execute on each filter in chain' do
+      tale_generator.chain.first.expects(:execute)
+      tale_generator.chain.last.expects(:execute)
+      tale_generator.run
+    end
+
+    it 'returns execute result of last filter in chain' do
+      tale_generator.run.must_be_kind_of Tale
     end
   end
 end
+
